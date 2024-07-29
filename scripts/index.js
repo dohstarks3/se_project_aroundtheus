36bae8e1ea4d7e4d8e2df96ae8317960680ec27a
Dimitri Starks <dimitristarks@Dimitris-Laptop.lan>
2024-07-29 14:59:32 -0500

    Sprint 5 Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,48 @@
 const initialCards = [
   {
+    // index = 0
+    id: 1,
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    liked: false,
   },
 
   {
+    // index = 1
+    id: 2,
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    liked: false,
   },
 
   {
+    // index = 2
+    id: 3,
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    liked: false,
   },
 
   {
+    // index = 3
+    id: 4,
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    liked: false,
   },
 
   {
+    id: 5,
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    liked: false,
   },
 
   {
+    id: 6,
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    liked: false,
   },
 ];
 
@@ -34,10 +50,12 @@ const editProfileModal = document.querySelector("#profile-edit-modal");
 const addCardModal = document.querySelector("#edit-card-modal");
 const addCardForm = addCardModal.querySelector(".modal__form");
 const addCardSaveButton = addCardModal.querySelector(".modal__save-button");
+const imageModalPreview = document.querySelector("#image-preview");
 
 const profileEditCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
@@ -54,6 +72,9 @@ const addCardButton = document.querySelector(".profile__add-button");
 const addCardCloseButton = addCardModal.querySelector(".modal__close-button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
+const imagePreviewCloseButton = imageModalPreview.querySelector(
+  ".modal__close-button"
+);
 
 //Event Handlers//
 
@@ -64,6 +85,43 @@ function handleProfileEditSubmit(e) {
   closePopup(editProfileModal);
 }
 
+function handleAddCardFormSubmit(e) {
+  e.preventDefault();
+
+  const cardName = document.querySelector("#add-title-input").value;
+  const cardLink = document.querySelector("#url-link-input").value;
+  console.log(cardName, cardLink);
+
+  const newCard = {
+    id: initialCards.length + 1,
+    name: cardName,
+    link: cardLink,
+    liked: false,
+  };
+
+  initialCards.push(newCard);
+  const cardElement = getCardElement(newCard);
+  cardListEl.prepend(cardElement);
+  closePopup(addCardModal);
+  addCardForm.reset();
+}
+
+function handleRemoveCardEl(e) {
+  const cardElement = e.target.closest(".card");
+  // get the name of the card
+  const cardName = cardElement.querySelector(
+    ".card__description-title"
+  ).textContent;
+  // find the card object with the corresponding name from the initialCards array, and remove it from the array
+  const cardIndex = initialCards.findIndex((card) => card.name === cardName);
+  // remove the element from the initialCards array
+  const deletedCard = initialCards.splice(cardIndex, 1);
+  // remove this card element from the DOM
+  cardElement.remove();
+  console.log(`The following card has been removed: ${deletedCard[0].name}`);
+  console.log(initialCards);
+}
+
 //Event Listeners//
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -72,8 +130,8 @@ profileEditButton.addEventListener("click", () => {
   editProfileModal.classList.add("modal_opened");
 });
 
-function closePopup(editProfileModal) {
-  editProfileModal.classList.remove("modal_opened");
+function closePopup(modal) {
+  modal.classList.remove("modal_opened");
 }
 
 addCardButton.addEventListener("click", () => {
@@ -81,31 +139,20 @@ addCardButton.addEventListener("click", () => {
 });
 
 profileEditCloseButton.addEventListener("click", () => {
-  editProfileModal.classList.remove("modal_opened");
+  closePopup(editProfileModal);
 });
 
 addCardCloseButton.addEventListener("click", () => {
-  addCardModal.classList.remove("modal_opened");
+  closePopup(addCardModal);
+});
+
+imagePreviewCloseButton.addEventListener("click", () => {
+  closePopup(imageModalPreview);
 });
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
-if (addCardModal.classList.contains("modal_opened")) {
-  addCardForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const cardName = document.querySelector("#card-title-input").value;
-    const cardLink = document.querySelector("#card-link-input").value;
-
-    const cardData = {
-      name: cardName,
-      link: cardLink,
-    };
-
-    const cardElement = getCardElement(cardData);
-    cardListEl.prepend(cardElement);
-    addCardModal.classList.remove("modal_opened");
-  });
-}
+addCardForm.addEventListener("submit", handleAddCardFormSubmit);
 
 //Card Functions//
 
@@ -116,12 +163,41 @@ function getCardElement(cardData) {
   //access the card title and image and store them in variables
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__description-title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteCardButton = cardElement.querySelector(".card__delete-button");
 
+  // Set the ID of the card element to the ID of the card object
+  cardElement.id = cardData.id;
   //set the path to the image to the link field of the object
   cardImageEl.src = cardData.link;
   //set the image alt text to the name field of the object\\
   cardTitleEl.textContent = cardData.name;
+  // Set the image alt text to the name field of the object
   cardImageEl.alt = cardData.name;
+  // Set the liked field of the card element to the liked field of the card object
+  cardElement.liked = cardData.liked;
+
+  if (cardData.liked === true) {
+    likeButton.classList.add("card__like-button_active");
+  } else {
+    likeButton.classList.remove("card__like-button_active");
+  }
+
+  likeButton.addEventListener("click", () => {
+    cardData.liked = !cardData.liked;
+    likeButton.classList.toggle("card__like-button_active");
+    console.log(`${cardData.name} liked: ${cardData.liked}`);
+  });
+
+  cardImageEl.addEventListener("click", () => {
+    // here we need to open the preview modal & pass inside the image card & name
+    document.querySelector(".modal__image").src = cardData.link;
+    document.querySelector("#image-preview").classList.add("modal_opened");
+    document.querySelector(".modal__caption").textContent = cardData.name;
+  });
+
+  deleteCardButton.addEventListener("click", handleRemoveCardEl);
+
   return cardElement;
 }
 
@@ -135,9 +211,22 @@ initialCards.forEach((cardData) => {
 
 //Like Button Function//
 
-const likeButtons = document.querySelectorAll(".card__like-button");
-likeButtons.forEach((likeButton) => {
-  likeButton.addEventListener("click", () => {
-    likeButton.classList.toggle("card__like-button_active");
-  });
-});
+// const likeButtons = document.querySelectorAll(".card__like-button");
+// likeButtons.forEach((likeButton) => {
+//   likeButton.addEventListener("click", () => {
+//     // likeButton.classList.toggle("card__like-button_active");
+//     // Get the ID of the card element that contains the like button
+
+//     // Get the CardData object with the corresponding ID
+//     const card = initialCards.find((card) => card.id === cardId);
+
+//     // Change the liked field of the card object
+//     card.liked = !card.liked;
+//     console.log(card.liked);
+//     // likeButton.classList.toggle("card__like-button_active");
+//     // if (card) {
+//     //   card.liked = !card.liked;
+//     //   likeButton.classList.toggle("card__like-button_active");
+//     // }
+//   });
+// });
