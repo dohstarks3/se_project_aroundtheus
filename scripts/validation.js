7955e6ef4f55c1d29437905e5aca88861f9da5f0
Dimitri Starks <dimitristarks@Dimitris-Laptop.lan>
2024-09-25 15:52:25 -0500

    Project Update

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,8 +1,43 @@
-enableValidation({
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
-});
+function setEventListeners(formEls, config) {
+  const { inputSelector } = config;
+  const inputEls = [...formEls.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEls) => {
+    inputEls.addEventListener("input", (e) => {
+      // console.log(inputEls.validity.valid);
+      if (inputEls.validity.valid) {
+      }
+    });
+  });
+}
+
+function enableValidation(config) {
+  const formEls = [...document.querySelectorAll(config.formSelector)];
+  formEls.forEach((formEls) => {
+    formEls.addEventListener(`submit`, (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEls, config);
+    //look for all inputs inside the form
+    //loop through all the inputs to see if all are valid
+    //if input is not valid
+    //get validation message
+    //add error class to input
+    //display error message
+    //disable button
+    //if all inputs are valid
+    //enable button
+    //reset error message
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
